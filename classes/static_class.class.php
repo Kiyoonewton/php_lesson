2b3faf4c936f5ce9479ce1e370cf627521a00bf8
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-07-06 19:04:04 +0100

    add calculation

--- classes/static_class.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Static_class
-{
-    public static $drinkingAge = 18;
-    public static function setDrinkageAge($age)
-    {
-        self::$drinkingAge = $age;
-    }
-}
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-include "includes/autoloader.inc.php"
+
+declare(strict_types=1);
+include "includes/class_autoloader.inc.php"
 ?>
 
 <!DOCTYPE html>
@@ -8,19 +10,22 @@ include "includes/autoloader.inc.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documented</title>
+    <title>Calculator</title>
 </head>
 
 <body>
-    <?php
-    $person = new Person("isaac", "blue", 21);
-    echo $person->getName();
-
-    echo Static_class::$drinkingAge;
-    echo Static_class::setDrinkageAge(21);
-    echo Static_class::$drinkingAge;
-
-    ?>
+    <form action="includes/calc.inc.php" method="post">
+        <p>My own calculator</p>
+        <input type="number" name="num1" placeholder="First number">
+        <select name="operator">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select>
+        <input type="number" name="num2" placeholder="Second number">
+        <button type="submit" name="submit">Calculate</button>
+    </form>
 </body>
 
 </html>
