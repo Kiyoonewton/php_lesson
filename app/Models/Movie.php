badaac71d52b9de6ea85bbcdaa3236fe1d27d3a3
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-07-21 04:20:50 +0100

    connection with mongodb

---
+++ app/Models/Movie.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+// use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+    protected $connection = 'mongodb';
+    protected $fillable = ['title', 'year', 'runtime', 'imdb', 'plot'];
+
+
+}
