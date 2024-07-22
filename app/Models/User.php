ad9f1391c708118c8a7bf91cd5a67a8fab3dc1b3
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-07-22 02:08:45 +0100

    drop everything not needed

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
-// use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
 
 class User extends Model
 {
     protected $connection = 'mongodb';
+    Use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    $data = User::all();
+    dd($data);
 });
 
 // Route::get('/browse_movies/', [MovieController::class, 'show']);
 
-
-Route::get('/info', function () {
-    phpinfo();
-});
